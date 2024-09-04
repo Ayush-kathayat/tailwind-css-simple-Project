9234c34d224738843d398e0f14f069bb0dc68ab4
Ayush-kathayat <kathayatayush789@gmail.com>
2024-09-04 12:05:08 +0530

    just started with the template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="h-screen bg-slate-600 flex items-center justify-center"> 
+    <h1 className="text-white ">Hello, I am Tailwind CSS.</h1>
+    </div>
+  )
+}
+
+export default App
